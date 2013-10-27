f82d95e518ddcaa07054b90b6fbd4d8624cffeea
db4soundman <db4soundman@gmail.com>
2013-10-27 16:32:18 -0400

    Added files, began attempting to implement the kd data structure. However, errors abound during class creation.

---
+++ blaseddStarbucks.h
@@ -0,0 +1,29 @@
+//
+//  blaseddStarbucks.h
+//  Starbucks
+//
+//  Created by Doug Blase on 10/25/13.
+//  Copyright (c) 2013 Doug Blase. All rights reserved.
+//
+
+#include "BST.h"
+#include "Starbucks.h"
+
+template <class Key, class T>
+class blaseddStarbucks: public Starbucks{
+public:
+    blaseddStarbucks();
+    ~blaseddStarbucks();
+    BST<Key, T> tree;
+    
+       virtual void build(Entry* c, int n);
+	
+       virtual Entry* getNearest(double x, double y);
+
+    
+};
+
+
+
+
+#include "blaseddStarbucks.cpp"
